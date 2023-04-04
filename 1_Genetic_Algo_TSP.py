09aeab53363f2f8ca9f97be426319f3392fa9963
BarVik27 <121300712+BarVik27@users.noreply.github.com>
2023-04-04 21:13:02 +0530

    Update 1_Genetic_Algo_TSP.py

--- 1_Genetic_Algo_TSP.py
+++ 1_Genetic_Algo_TSP.py
@@ -1,173 +1,107 @@
-from random import randint
- 
-INT_MAX = 2147483647
+import random
+import numpy as np
+import matplotlib.pyplot as plt
 
-V = 5
- 
 
-GENES = "ABCDE"
- 
+cities = {
+    'A': (1, 1),
+    'B': (3, 1),
+    'C': (3, 3),
+    'D': (1, 3),
+    'E': (2, 2)
+}
 
-START = 0
- 
-POP_SIZE = 10
- 
 
- 
- 
-class individual:
-    def __init__(self) -> None:
-        self.gnome = ""
-        self.fitness = 0
- 
-    def __lt__(self, other):
-        return self.fitness < other.fitness
- 
-    def __gt__(self, other):
-        return self.fitness > other.fitness
- 
- 
+POPULATION_SIZE = 50
+ELITE_SIZE = 5
+MUTATION_RATE = 0.01
+NUM_GENERATIONS = 1000
 
-def rand_num(start, end):
-    return randint(start, end-1)
- 
- 
 
-def repeat(s, ch):
-    for i in range(len(s)):
-        if s[i] == ch:
-            return True
- 
-    return False
- 
- 
+def calc_distance(city1, city2):
+    x1, y1 = city1
+    x2, y2 = city2
+    return np.sqrt((x1-x2)**2 + (y1-y2)**2)
 
-def mutatedGene(gnome):
-    gnome = list(gnome)
-    while True:
-        r = rand_num(1, V)
-        r1 = rand_num(1, V)
-        if r1 != r:
-            temp = gnome[r]
-            gnome[r] = gnome[r1]
-            gnome[r1] = temp
-            break
-    return ''.join(gnome)
- 
- 
+def calc_path_distance(path):
+    distance = 0
+    for i in range(len(path)-1):
+        distance += calc_distance(cities[path[i]], cities[path[i+1]])
+    distance += calc_distance(cities[path[-1]], cities[path[0]])
+    return distance
 
-def create_gnome():
-    gnome = "0"
-    while True:
-        if len(gnome) == V:
-            gnome += gnome[0]
-            break
- 
-        temp = rand_num(1, V)
-        if not repeat(gnome, chr(temp + 48)):
-            gnome += chr(temp + 48)
- 
-    return gnome
- 
- 
+def calc_fitness(path):
+    return 1 / calc_path_distance(path)
 
-def cal_fitness(gnome):
-    mp = [
-        [0, 2, INT_MAX, 12, 5],
-        [2, 0, 4, 8, INT_MAX],
-        [INT_MAX, 4, 0, 3, 3],
-        [12, 8, 3, 0, 10],
-        [5, INT_MAX, 3, 10, 0],
-    ]
-    f = 0
-    for i in range(len(gnome) - 1):
-        if mp[ord(gnome[i]) - 48][ord(gnome[i + 1]) - 48] == INT_MAX:
-            return INT_MAX
-        f += mp[ord(gnome[i]) - 48][ord(gnome[i + 1]) - 48]
- 
-    return f
- 
- 
 
-def cooldown(temp):
-    return (90 * temp) / 100
- 
- 
+def create_individual():
+    path = list(cities.keys())
+    random.shuffle(path)
+    return path
 
-def TSPUtil(mp):
-    # Generation Number
-    gen = 1
-    # Number of Gene Iterations
-    gen_thres = 5
- 
+def create_population(size):
     population = []
-    temp = individual()
- 
-    # Populating the GNOME pool.
-    for i in range(POP_SIZE):
-        temp.gnome = create_gnome()
-        temp.fitness = cal_fitness(temp.gnome)
-        population.append(temp)
- 
-    print("\nInitial population: \nGNOME     FITNESS VALUE\n")
-    for i in range(POP_SIZE):
-        print(population[i].gnome, population[i].fitness)
-    print()
- 
-    found = False
-    temperature = 10000
- 
+    for i in range(size):
+        population.append(create_individual())
+    return population
+
+def rank_population(population):
+    ranked_population = sorted(population, key=calc_fitness, reverse=True)
+    return ranked_population
+
+def select_parents(population):
+    parent1 = random.choice(population[:ELITE_SIZE])
+    parent2 = random.choice(population[:ELITE_SIZE])
+    return parent1, parent2
+
+def crossover(parent1, parent2):
    
-    while temperature > 1000 and gen <= gen_thres:
-        population.sort()
-        print("\nCurrent temp: ", temperature)
-        new_population = []
- 
-        for i in range(POP_SIZE):
-            p1 = population[i]
- 
-            while True:
-                new_g = mutatedGene(p1.gnome)
-                new_gnome = individual()
-                new_gnome.gnome = new_g
-                new_gnome.fitness = cal_fitness(new_gnome.gnome)
- 
-                if new_gnome.fitness <= population[i].fitness:
-                    new_population.append(new_gnome)
+    start = random.randint(0, len(parent1)-1)
+    end = random.randint(start, len(parent1)-1)
+    child = [None] * len(parent1)
+    for i in range(start, end+1):
+        child[i] = parent1[i]
+    for i in range(len(parent2)):
+        if parent2[i] not in child:
+            for j in range(len(child)):
+                if child[j] is None:
+                    child[j] = parent2[i]
                     break
- 
-                else:
- 
-                    
-                    prob = pow(
-                        2.7,
-                        -1
-                        * (
-                            (float)(new_gnome.fitness - population[i].fitness)
-                            / temperature
-                        ),
-                    )
-                    if prob > 0.5:
-                        new_population.append(new_gnome)
-                        break
- 
-        temperature = cooldown(temperature)
-        population = new_population
-        print("Generation", gen)
-        print("GNOME     FITNESS VALUE")
- 
-        for i in range(POP_SIZE):
-            print(population[i].gnome, population[i].fitness)
-        gen += 1
- 
- 
-if __name__ == "__main__":
- 
-    mp = [
-        [0, 2, INT_MAX, 12, 5],
-        [2, 0, 4, 8, INT_MAX],
-        [INT_MAX, 4, 0, 3, 3],
-        [12, 8, 3, 0, 10],
-        [5, INT_MAX, 3, 10, 0],
-    ]
-    TSPUtil(mp)
+    return child
+
+def mutate(individual):
+   
+    for i in range(len(individual)):
+        if random.random() < MUTATION_RATE:
+            j = random.randint(0, len(individual)-1)
+            individual[i], individual[j] = individual[j], individual[i]
+    return individual
+
+def create_new_population(population):
+    new_population = []
+    while len(new_population) < len(population):
+        parent1, parent2 = select_parents(population)
+        child = crossover(parent1, parent2)
+        child = mutate(child)
+        new_population.append(child)
+    return new_population
+
+
+population = create_population(POPULATION_SIZE)
+best_distances = []
+for i in range(NUM_GENERATIONS):
+    population = rank_population(population)
+    best_path = population[0]
+    best_distance = calc_path_distance(best_path)
+    best_distances.append(best_distance)
+    print(f'Generation {i}: Best Distance = {best_distance}')
+    population = create_new_population(population)
+
+
+plt.plot(best_distances)
+plt.xlabel('Generation')
+plt.ylabel('Best Distance')
+plt.show()
+
+
+best_path.append(best_path[0]) # close the
