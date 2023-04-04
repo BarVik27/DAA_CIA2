e7735bcfe46c21ac50bebb20f151f3628acd64ef
BarVik27 <121300712+BarVik27@users.noreply.github.com>
2023-04-04 21:28:29 +0530

    Update 5_ant_colony_optimization.py

--- 5_ant_colony_optimization.py
+++ 5_ant_colony_optimization.py
@@ -1,77 +1,61 @@
 import numpy as np
+import random
 
-class AntColony:
-    def __init__(self, size, density, start, end, alpha=1, beta=2, evap_rate=0.5, generations=100):
-        self.size = size
-        self.density = density
-        self.start = start
-        self.end = end
-        self.alpha = alpha
-        self.beta = beta
-        self.evap_rate = evap_rate
-        self.generations = generations
+#  adjacency matrix
+graph = np.array([[0, 2, 3, 0, 0],
+                  [2, 0, 0, 5, 6],
+                  [3, 0, 0, 7, 0],
+                  [0, 5, 7, 0, 4],
+                  [0, 6, 0, 4, 0]])
+
+
+num_ants = 10
+alpha = 1.0
+beta = 2.0
+rho = 0.5
+Q = 1.0
+
+
+pheromone = np.ones(graph.shape) / len(graph)
+
+
+def path_length(path):
+    length = 0
+    for i in range(len(path)-1):
+        length += graph[path[i]][path[i+1]]
+    return length
+
+
+def update_pheromone(paths, lengths):
+    pheromone *= (1-rho)
+    for i in range(len(paths)):
+        for j in range(len(paths[i])-1):
+            pheromone[paths[i][j]][paths[i][j+1]] += Q/lengths[i]
+
+
+for i in range(100):
+    ant_paths = []
+    ant_lengths = []
+    for j in range(num_ants):
         
-        self.graph = self.generate_problem(size, density)
-        self.pheromones = np.ones((size, size))
-        
-    def generate_problem(self, size, density):
-        graph = np.full((size, size), np.inf)
-        for i in range(size):
-            for j in range(i, size):
-                if np.random.random() < density and i != j:
-                    w = np.random.randint(1, 20)
-                    graph[i][j] = w
-                    graph[j][i] = w
-        return graph
-        
-    def choose_next_vertex(self, curr_pos, visited):
-        unvisited = np.where(visited == False)[0]
-        graph = self.graph[curr_pos, unvisited]
-        pheromones = self.pheromones[curr_pos, unvisited]
-        weights = np.power(pheromones, self.alpha) * np.power(1 / graph, self.beta)
-        weights /= np.sum(weights)
-        next_vertex = np.random.choice(unvisited, p=weights)
-        return next_vertex
-        
-    def traverse(self):
-        curr_pos = self.start
-        path = [curr_pos]
-        visited = np.zeros(self.size, dtype=bool)
-        visited[curr_pos] = True
-        cost = 0
-        
-        while curr_pos != self.end:
-            next_vertex = self.choose_next_vertex(curr_pos, visited)
-            visited[next_vertex] = True
-            cost += self.graph[curr_pos][next_vertex]
-            path.append(next_vertex)
-            curr_pos = next_vertex
+        path = [random.randint(0, len(graph)-1)]
+       
+        while len(path) < len(graph):
             
-        return path, cost
+            prob = pheromone[path[-1]] ** alpha * (1/graph[path[-1]]) ** beta
+            prob /= prob.sum()
+           
+            next_node = np.random.choice(range(len(graph)), p=prob)
+          
+            path.append(next_node)
+       
+        length = path_length(path)
         
-    def release_generation(self):
-        paths, costs = [], []
-        for i in range(self.size):
-            path, cost = self.traverse()
-            paths.append(path)
-            costs.append(cost)
-        return paths, costs
-        
-    def update_pheromones(self, paths, costs):
-        self.pheromones *= (1 - self.evap_rate)
-        for path, cost in zip(paths, costs):
-            for i in range(len(path) - 1):
-                curr_pos, next_pos = path[i], path[i+1]
-                self.pheromones[curr_pos][next_pos] += 1 / cost
-        
-    def run(self):
-        for i in range(self.generations):
-            paths, costs = self.release_generation()
-            self.update_pheromones(paths, costs)
-            best_path, best_cost = paths[np.argmin(costs)], np.min(costs)
-            print(f"Generation {i+1}, Best Path: {best_path}, Cost: {best_cost}")
-
-
-
-ac = AntColony(size=10, density=0.5, start=0, end=7)
-ac.run()
+        ant_paths.append(path)
+        ant_lengths.append(length)
+   
+    update_pheromone(ant_paths, ant_lengths)
+#  shortest path
+shortest_path = ant_paths[np.argmin(ant_lengths)]
+print('Shortest Path:', shortest_path)
+print('Shortest Path Length:', min(ant_lengths))
